aefbec6f09393cfe0c2be906eec6514a0b199677
aqsa12345678 <bt20125@qmul.ac.uk>
2025-03-03 12:14:10 +0000

    Update Base_query.sql

--- database/Base_query.sql
+++ database/Base_query.sql
@@ -1,24 +1,29 @@
 SELECT 
-	SNP_Information.RSID,
-	SNP_Information.Chromosome,
-	SNP_Information.Position,
-	SNP_Information.Alternate_Allele,
-	SNP_Information.Reference_Allele,
-	SNP_Information.Mapped_genes,
-	SNP_Information.Phenotype,
-	CONCAT(
-	ROUND(SNP_Information.P_value / POW(10, FLOOR(LOG10(SNP_Information.P_value))), 2),
-	'e',
-	FLOOR(LOG10(SNP_Information.P_value))
-	) AS P_value,  -- changes the notation of the p-value so that are written in scientific notation with 2 decimal places
-	SNP_Information.Ancestry,
-	Statistics.Population_ID,
-	Comparative_Statistics.Tajima_D,
-	Comparative_Statistics.Haplotype_Diversity,
-	Comparative_Statistics.Nucleotide_Diversity
+    s.SNP_ID,
+    s.RSID,
+    s.Chromosome,
+    s.Position,
+    s.Reference_Allele,
+    s.Alternate_Allele,
+    CONCAT(
+        ROUND(si.P_value / POW(10, FLOOR(LOG10(si.P_value))), 2),
+        'e',
+        FLOOR(LOG10(si.P_value))
+    ) AS P_value,
+    s.Mapped_genes,  
+    s.Phenotype,
+    COALESCE(st.Population_ID, 'N/A') AS Population,
+    cs.FST_Score,
+    cs.XPEHH,
+    st.Tajima_D,
+    st.Haplotype_Diversity,
+    st.Nucleotide_Diversity,
+    s.Ancestry
 FROM 
-	SNP_Information
+    SNP_Information s
 LEFT JOIN 
-	Statistics ON SNP_Information.SNP_ID = Statistics.SNP_ID
-LEFT JOIN 
-	Comparative_Statistics ON SNP_Information.SNP_ID = Comparative_Statistics.SNP_ID
+    Statistics st ON s.SNP_ID = st.SNP_ID
+LEFT JOIN
+	Population p ON st.Population_ID = p.Population_ID
+LEFT JOIN
+	Comparative_Statistics cs ON s.SNP_ID = cs.SNP_ID
