8a088dbfee5185736bcaecd1e3f8f895b59eb8b2
aqsa12345678 <bt20125@qmul.ac.uk>
2025-03-05 15:14:57 +0000

    Update Base_query.sql

--- database/Base_query.sql
+++ database/Base_query.sql
@@ -1,26 +1,26 @@
 SELECT 
-    s.SNP_ID,
-    s.RSID,
-    s.Chromosome,
-    s.Position,
-    s.Reference_Allele,
-    s.Alternate_Allele,
-    CONCAT(
-        ROUND(s.P_value / POW(10, FLOOR(LOG10(si.P_value))), 2),'e', FLOOR(LOG10(s.P_value))
+	s.SNP_ID,
+	s.RSID,
+	s.Chromosome,
+	s.Position,
+	s.Reference_Allele,
+	s.Alternate_Allele,
+	CONCAT(
+        	ROUND(s.P_value / POW(10, FLOOR(LOG10(s.P_value))), 2),'e', FLOOR(LOG10(s.P_value))
 	) AS P_value,
-    s.Mapped_genes,  
-    s.Phenotype,
-    COALESCE(st.Population_ID, 'N/A') AS Population,
-    cs.FST_Score,
-    cs.XPEHH,
-    st.Tajima_D,
-    st.Haplotype_Diversity,
-    st.Nucleotide_Diversity,
-    s.Ancestry
+    	s.Mapped_genes,  
+    	s.Phenotype,
+    	COALESCE(st.Population_ID, 'N/A') AS Population,
+    	cs.FST_Score,
+    	cs.XPEHH,
+    	st.Tajima_D,
+    	st.Haplotype_Diversity,
+    	st.Nucleotide_Diversity,
+    	s.Ancestry
 FROM 
-    SNP_Information s
+	SNP_Information s
 LEFT JOIN 
-    Statistics st ON s.SNP_ID = st.SNP_ID
+	Statistics st ON s.SNP_ID = st.SNP_ID
 LEFT JOIN
 	Population p ON st.Population_ID = p.Population_ID
 LEFT JOIN
